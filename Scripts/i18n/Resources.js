b5e2a39abcc3ce80f095baafac00f2647888854b
knopa <klepa.klepa@gmail.com>
2016-05-02 20:49:55 +0300

    v.0.1.5

--- Scripts/i18n/Resources.js
+++ Scripts/i18n/Resources.js
@@ -165,7 +165,8 @@ i18n.Resources = (function () {
   "Add": "Add",
   "ComplexEquipment": "Suit",
   "ComplexEquipments": "Suits",
-  "EnterComplexEquipment": "Enter a MAC address, a physical location, or a serial #"
+  "EnterComplexEquipment": "Enter a MAC address, a physical location, or a serial #",
+  "Admin": "Admin"
 };
 	return $.extend({}, i18n.Resources || {}, strings);
 }());
